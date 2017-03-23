73e708e9c74341abe45ee625ccb9a66d21645be8
ISpectruM <ispectrumcode@gmail.com>
2017-03-24 00:03:04 +0200

    Problem 8.	Custom List Sorter - added

---
+++ JavaOOPAdvanced/src/B_Generics_Exercises/G_CustomListSorter/CustomList.java
@@ -0,0 +1,12 @@
+package B_Generics_Exercises.G_CustomListSorter;
+
+public interface CustomList<T extends Comparable<T>> {
+
+    void add(T element);
+    T remove(int index);
+	boolean contains(T element);
+	void swap(int firstIndex, int secondIndex);
+	int countGreaterThat(T element);
+	T getMax();
+	T getMin();
+}
