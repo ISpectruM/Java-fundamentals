cd186681d9f48e7a891a851b2c62740d6df496c5
ISpectruM <ispectrumcode@gmail.com>
2017-03-29 10:34:20 +0300

    4.	Create Annotation - added

---
+++ JavaOOPAdvanced/src/D_EnumsAndAnnotations_Lab/E_CreateAnnotation/Main.java
@@ -0,0 +1,9 @@
+package D_EnumsAndAnnotations_Lab.E_CreateAnnotation;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
---
+++ JavaOOPAdvanced/src/D_EnumsAndAnnotations_Lab/E_CreateAnnotation/TestClass.java
@@ -0,0 +1,5 @@
+package D_EnumsAndAnnotations_Lab.E_CreateAnnotation;
+
+@Subject(categories = {"Test","Annotations"})
+public class TestClass {
+}
